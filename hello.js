222753ace86927faa83f2da3b87e864a35680375
Madhav Menon <madhavmenon2605@gmail.com>
2021-08-16 13:11:55 +0530

    Tweaked index.html

--- hello.js
+++ hello.js
@@ -11,7 +11,7 @@ http.createServer(function(req,res){
 	fs.readFile(filename,function(err,data){
 		if (err){
 			res.writeHead(404,{'Content-Type': 'text/html'});
-			return res.end("404 check your url motherfucker");
+			return res.end("404 Not Found");
 		}
 	  res.writeHead(200, {'Content-Type': 'text/html'});
 	  res.write(data);
